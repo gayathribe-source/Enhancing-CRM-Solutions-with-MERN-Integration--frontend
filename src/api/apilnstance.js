a8210839f35bd3e517a47010b8f61e27933a74c9
Gayathri Jiju <gayathribe26@gmail.com>
2026-07-08 14:52:05 +0530

    Frontend completed

--- src/api/apilnstance.js
+++ src/api/apilnstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
-  withCredentials: true,
+  baseURL: "https://mern-integration-backend-rfws.onrender.com",
+  withCredentials: true
 });
 
 export default api;
